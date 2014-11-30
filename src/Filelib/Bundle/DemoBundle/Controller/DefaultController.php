8a7a9e741861de1596cbd78ebc011f80931870b0
Janimatti Ellonen <janimatti.ellonen@soprano.fi>
2014-11-30 17:34:23 +0200

    Removed non-working actions for now + cleaned up example index page

--- src/Filelib/Bundle/DemoBundle/Controller/DefaultController.php
+++ src/Filelib/Bundle/DemoBundle/Controller/DefaultController.php
@@ -11,8 +11,7 @@ use Xi\Filelib\FileLibrary;
 
 class DefaultController extends Controller
 {
-
-    public function clearAction()
+	public function clearAction()
     {
         $filelib = $this->getFilelib();
 
@@ -27,212 +26,18 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $filelib = $this->getFilelib();
+
         // We want to upload curious manatee image.
         $path = $this->get('kernel')->getRootDir() . "/data/uploads/west_indian_manatee_and_nursing_calf_crystal_river_florida.jpg";
 
-		//die($path);
 		$file = $filelib->uploadFile($path);
-		//print_r($file);die;
 		$publisher = $this->get('xi_filelib.publisher');
 
 		$publisher->publishAllVersions($file);
 
-		/*
-        // Find root folder
-        $folder = $filelib->getFolderOperator()->findRoot();
-
-        // Prepare file for upload
-        $upload = $filelib->getFileOperator()->prepareUpload($path);
-
-
-        // Configure (optional) limiter to accept only images
-        $limiter = new \Xi\Filelib\File\Upload\Limiter();
-        $limiter->accept('image/');
-
-        // If not accepted by limiter, deny upload.
-        if (!$limiter->isAccepted($upload)) {
-            throw new \Symfony\Component\HttpKernel\Exception\HttpException(403, "File type '{$upload->getMimeType()}' is not allowed");
-        }
-
-
-        $op = $filelib->getFileOperator();
-
-        $file = $op->upload($upload, $folder, 'versioned');
-		*/
-
         return $this->render('FilelibDemoBundle:Default:index.html.twig', array(
-            'fl' => $filelib,
             'file' => $file,
         ));
-
-        return $this->render('FilelibDemoBundle:Default:index.html.twig');
-
-    }
-
-
-
-
-    public function selfishAction()
-    {
-        $filelib = $this->getFilelib();
-
-        // We want to upload curious manatee image.
-        $path = $this->get('kernel')->getRootDir() . "/data/uploads/west_indian_manatee_and_nursing_calf_crystal_river_florida.jpg";
-
-        // Find root folder
-        $folder = $filelib->getFolderOperator()->findRoot();
-
-        // Prepare file for upload
-        $upload = $filelib->getFileOperator()->prepareUpload($path);
-
-        $op = $filelib->getFileOperator();
-
-        $file = $op->upload($upload, $folder, 'selfish');
-
-        return $this->render('FilelibDemoBundle:Default:selfish.html.twig', array(
-            'fl' => $filelib,
-            'file' => $file,
-        ));
-
-        return $this->render('FilelibDemoBundle:Default:selfish.html.twig');
-
-    }
-
-
-
-    public function asyncUploadTestAction()
-    {
-        $filelib = $this->getFilelib();
-
-        $iter = new \DirectoryIterator($this->get('kernel')->getRootDir() . '/data/uploads');
-
-        $folder = $filelib->getFolderOperator()->findRoot();
-
-        $uploaded = array();
-
-        $filelib->getFileOperator()->setCommandStrategy(DefaultFileOperator::COMMAND_UPLOAD, Command::STRATEGY_ASYNCHRONOUS);
-
-        for ($x = 1; $x <= 10; $x++) {
-            foreach ($iter as $key => $file) {
-                if ($file->isFile()) {
-                    $filelib->getFileOperator()->upload($file->getRealPath(), $folder, 'versioned');
-                }
-            }
-        }
-
-        return new Response('Some uploads were created, sir');
-    }
-
-
-    public function copyTestAction()
-    {
-        $filelib = $this->getFilelib();
-
-        // We want to upload curious manatee image.
-        $path = $this->get('kernel')->getRootDir() . "/data/uploads/curious-manatee.jpg";
-
-        // Find root folder
-        $folder = $filelib->getFolderOperator()->findRoot();
-
-        // Prepare file for upload
-        $upload = $filelib->getFileOperator()->prepareUpload($path);
-
-        $file = $filelib->getFileOperator()->upload($upload, $folder, 'versioned');
-        $file2 = $filelib->getFileOperator()->upload($upload, $folder, 'selfish');
-
-        $file3 = $filelib->getFileOperator()->copy($file, $folder);
-        $file4 = $filelib->getFileOperator()->copy($file2, $folder);
-
-        echo "<pre>";
-        var_dump($file);
-        var_dump($file3);
-
-        echo "<hr />";
-        var_dump($file2);
-        var_dump($file4);
-        die();
-
-    }
-
-
-    public function testSuiteAction()
-    {
-        $filelib = $this->getFilelib();
-
-        $root = $filelib->getFolderOperator()->findRoot();
-
-        $fitem = $filelib->getFolderOperator()->getInstance();
-        $fitem->setName('tussi');
-        $fitem->setParentId($root->getId());
-
-        $filelib->getFolderOperator()->create($fitem);
-
-        $fitem2 = $filelib->getFolderOperator()->getInstance();
-        $fitem2->setName('lussi');
-        $fitem2->setParentId($fitem->getId());
-        $filelib->getFolderOperator()->create($fitem2);
-
-        $fitem->setName('mustekasetti');
-        $filelib->getFolderOperator()->update($fitem);
-
-        $iter = new \DirectoryIterator($this->get('kernel')->getRootDir() . '/data/uploads');
-
-
-        $uploaded = array();
-
-        foreach ($iter as $key => $file) {
-            if ($file->isFile()) {
-
-                if ($key % 2 == 1) {
-                    $folder = $fitem;
-                } else {
-                    $folder = $fitem2;
-                }
-
-                $uploaded[] = $filelib->getFileOperator()->upload($file->getRealPath(), $folder, 'versioned');
-            }
-        }
-
-
-        $fitem2->setName('lusauttaja');
-        $filelib->getFolderOperator()->update($fitem2);
-
-        foreach($uploaded as $file) {
-
-            $file->setName(strrev($file->getName()));
-            $filelib->getFileOperator()->update($file);
-        }
-
-        $filelib->getFolderOperator()->delete($fitem);
-
-        return new Response('Test suite completed');
-
-    }
-
-
-
-    public function videoTestAction()
-    {
-        $filelib = $this->getFilelib();
-
-        // We want to upload curious manatee image.
-        $path = $this->get('kernel')->getRootDir() . "/../vendor/xi-filelib/tests/data/hauska-joonas.mp4";
-
-        // Find root folder
-        $folder = $filelib->getFolderOperator()->findRoot();
-
-        // Prepare file for upload
-        $upload = $filelib->getFileOperator()->prepareUpload($path);
-
-
-        $op = $filelib->getFileOperator();
-
-        $op->setCommandStrategy(DefaultFileOperator::COMMAND_UPLOAD, Command::STRATEGY_ASYNCHRONOUS);
-
-        $file = $op->upload($upload, $folder, 'versioned');
-
-        return new Response('Video upload was pooped to the queue');
-
     }
 
     /**
@@ -242,7 +47,4 @@ class DefaultController extends Controller
     {
         return $this->get('xi_filelib');
     }
-
-
-
 }
